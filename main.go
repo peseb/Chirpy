d68cbba898ce5daf57feb89ec1285b722bbba812
Petter Bjørkås <pbjorkaas@enua.no>
2025-09-15 07:41:58 +0200

    Add health endpoint

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ func main() {
 	}
 
 	fileHandler := http.FileServer(http.Dir("."))
-	serveMux.Handle("/", fileHandler)
+	serveMux.Handle("/app/", http.StripPrefix("/app", fileHandler))
+	serveMux.HandleFunc("/healthz", healthHandler)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
 }
