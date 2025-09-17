2b2f8b63d631dd7ae07e1473531c3781994551c5
Petter Bjørkås <pbjorkaas@enua.no>
2025-09-17 07:50:32 +0200

    Specify allowed methods

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics() int32 {
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *apiConfig) resetMetrics() {
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	serveMux := http.NewServeMux()
 	server := http.Server{
 		Handler: serveMux,
@@ -13,12 +37,28 @@ func main() {
 	}
 
 	fileHandler := http.FileServer(http.Dir("."))
-	serveMux.Handle("/app/", http.StripPrefix("/app", fileHandler))
-	serveMux.HandleFunc("/healthz", healthHandler)
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileHandler)))
+	serveMux.HandleFunc("GET /healthz", handlerHealth)
+	serveMux.HandleFunc("GET /metrics", cfg.handlerMetrics)
+	serveMux.HandleFunc("POST /reset", cfg.handlerReset)
 	log.Fatal(server.ListenAndServe())
 }
 
-func healthHandler(rw http.ResponseWriter, req *http.Request) {
+func (cfg *apiConfig) handlerMetrics(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	res := fmt.Sprintf("Hits: %v", cfg.getMetrics())
+	rw.Write([]byte(res))
+}
+
+func (cfg *apiConfig) handlerReset(rw http.ResponseWriter, req *http.Request) {
+	cfg.resetMetrics()
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerHealth(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader(200)
 	rw.Write([]byte("OK"))
